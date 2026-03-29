fe17ecd443fe396036c413f9c23840a29ffb1372
Sadem Hussen <154666429+crackasuper@users.noreply.github.com>
2026-03-29 04:35:48 +0300

    Implement sorting for mixed odd and even numbers

---
+++ ehab_is_an_odd_person.py
@@ -0,0 +1,11 @@
+
+n = int(input())
+a = list(map(int, input().split()))
+
+has_odd = any(x % 2 == 1 for x in a)
+has_even = any(x % 2 == 0 for x in a)
+
+if has_odd and has_even:
+    a.sort()
+
+print(*a) # GOD! it's 4:34 at the night.
