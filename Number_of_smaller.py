cacf3f5053a5f267dd58968ea33f334eb76dce8b
Sadem Hussen <154666429+crackasuper@users.noreply.github.com>
2026-03-29 03:49:46 +0300

    finding number less or equal to number in second array

---
+++ Number_of_smaller.py
@@ -0,0 +1,14 @@
+
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+i = 0
+res = []
+
+for x in b:
+    while i < n and a[i] < x:
+        i += 1
+    res.append(str(i))
+
+print(" ".join(res))
